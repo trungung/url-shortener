0f07e1f803b1296d5746d3eedc288bca4e1c343d
ung-trung <ungkientrung@gmail.com>
2025-05-19 16:21:33 +0300

    add persisted state local dev

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,5 +6,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    cloudflare({
+      persistState: {
+        path: "../../.wrangler",
+      },
+    }),
+  ],
 });
